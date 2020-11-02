8368b8017bddd4d9be11e9a31d65f3754ea91a51
Moritz Haller <mhaller@brave.com>
2020-11-02 09:55:56 +0000

    update seed for testing

--- serialize_variations_seed.py
+++ serialize_variations_seed.py
@@ -92,24 +92,24 @@ def serialize_and_save_variations_seed_message(seed_data, path):
         #     study.filter.end_date = string_to_timestamp(study_data['filter']['end_date_utc'])
 
         # TODO: Delete all but STABLE
-        study.filter.channel.append(study_pb2.Study.Channel.UNKNOWN)
-        study.filter.channel.append(study_pb2.Study.Channel.CANARY)
-        study.filter.channel.append(study_pb2.Study.Channel.DEV)
-        study.filter.channel.append(study_pb2.Study.Channel.BETA)
-        study.filter.channel.append(study_pb2.Study.Channel.STABLE)
+        # study.filter.channel.append(study_pb2.Study.Channel.UNKNOWN)
+        # study.filter.channel.append(study_pb2.Study.Channel.CANARY)
+        # study.filter.channel.append(study_pb2.Study.Channel.DEV)
+        # study.filter.channel.append(study_pb2.Study.Channel.BETA)
+        # study.filter.channel.append(study_pb2.Study.Channel.STABLE)
 
-        for platform in study_data['filter']['platform']:
-            supported_platforms = {
-                'WINDOWS': study_pb2.Study.Platform.PLATFORM_WINDOWS,
-                'MAC': study_pb2.Study.Platform.PLATFORM_MAC,
-                'LINUX': study_pb2.Study.Platform.PLATFORM_LINUX,
-                'IOS': study_pb2.Study.Platform.PLATFORM_IOS,
-                'ANDROID': study_pb2.Study.Platform.PLATFORM_ANDROID
-            }
-            study.filter.platform.append(supported_platforms[platform])
+        # for platform in study_data['filter']['platform']:
+        #     supported_platforms = {
+        #         'WINDOWS': study_pb2.Study.Platform.PLATFORM_WINDOWS,
+        #         'MAC': study_pb2.Study.Platform.PLATFORM_MAC,
+        #         'LINUX': study_pb2.Study.Platform.PLATFORM_LINUX,
+        #         'IOS': study_pb2.Study.Platform.PLATFORM_IOS,
+        #         'ANDROID': study_pb2.Study.Platform.PLATFORM_ANDROID
+        #     }
+        #     study.filter.platform.append(supported_platforms[platform])
 
-        for country in study_data['filter']['country']:
-            study.filter.country.append(country)
+        # for country in study_data['filter']['country']:
+        #     study.filter.country.append(country)
 
     # Serialize and save
     with open(path, "wb") as file:
