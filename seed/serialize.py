b9b0b084f077b758c6368464d8d472ebd6045045
Ivan Efremov <iefremov@brave.com>
2021-04-09 14:51:07 +0700

    Introduce max version filter.

--- seed/serialize.py
+++ seed/serialize.py
@@ -125,6 +125,9 @@ def serialize_and_save_variations_seed_message(seed_data, path):
         if 'min_version' in study_data['filter']:
             study.filter.min_version = study_data['filter']['min_version']
 
+        if 'max_version' in study_data['filter']:
+            study.filter.max_version = study_data['filter']['max_version']
+
     # Serialize and save
     with open(path, "wb") as file:
         file.write(seed.SerializeToString())
