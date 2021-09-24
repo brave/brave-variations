eb267a00fd170ebac4fc1f404bc40b7ffa5365e6
Terry Mancey <terry.mancey@icloud.com>
2021-09-24 12:01:54 -0500

    Add support for min_os_version and max_os_version
    # Conflicts:
    #	seed/serialize.py

--- seed/serialize.py
+++ seed/serialize.py
@@ -125,6 +125,15 @@ def serialize_and_save_variations_seed_message(seed_data, path):
         if 'min_version' in study_data['filter']:
             study.filter.min_version = study_data['filter']['min_version']
 
+        if 'max_version' in study_data['filter']:
+            study.filter.max_version = study_data['filter']['max_version']
+
+        if 'min_os_version' in study_data['filter']:
+            study.filter.min_os_version = study_data['filter']['min_os_version']
+
+        if 'max_os_version' in study_data['filter']:
+            study.filter.max_os_version = study_data['filter']['max_os_version']
+
     # Serialize and save
     with open(path, "wb") as file:
         file.write(seed.SerializeToString())
