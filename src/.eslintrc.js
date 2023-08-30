9509d39b23d8a767ca65765c0f759d595aa7babf
Mikhail Atuchin <matuchin@brave.com>
2023-08-30 21:04:52 +0600

    Add licenses

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -1,6 +1,10 @@
+// Copyright (c) 2023 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
 module.exports = {
   extends: ['standard-with-typescript', 'prettier'],
-  plugins: ['prettier'],
+  plugins: ['prettier', 'licenses'],
 
   root: true,
   parserOptions: {
@@ -19,6 +23,22 @@ module.exports = {
       {
         singleQuote: true,
         printWidth: 80,
+      },
+    ],
+    'licenses/header': [
+      2,
+      {
+        tryUseCreatedYear: true,
+        comment: {
+          allow: 'both',
+          prefer: 'line',
+        },
+        header: [
+          'Copyright (c) {YEAR} The Brave Authors. All rights reserved.',
+          'This Source Code Form is subject to the terms of the Mozilla Public',
+          'License, v. 2.0. If a copy of the MPL was not distributed with this file,',
+          'You can obtain one at https://mozilla.org/MPL/2.0/.',
+        ],
       },
     ],
   },
--- src/web/src/app.ts
+++ src/web/src/app.ts
@@ -1,3 +1,7 @@
+// Copyright (c) 2021 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
 import Vue from 'vue';
 import { variations as proto } from '../../proto/generated/proto_bundle';
 import { ProcessedStudy, StudyPriority } from '../../core/study_classifier';
