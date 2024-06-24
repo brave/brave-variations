5a4fa66f7191112c74da3d1b5c934346b0aed6cc
Aleksey Khoroshilov <akhoroshilov@brave.com>
2024-06-24 19:36:31 +0700

    Handle exclude_ filter fields correctly.

--- src/seed_tools/utils/seed_validation.ts
+++ src/seed_tools/utils/seed_validation.ts
@@ -4,9 +4,9 @@
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
 import DefaultMap from '../../base/containers/default_map';
-import { type Study, type Study_Filter } from '../../proto/generated/study';
+import { type Study_Filter } from '../../proto/generated/study';
 import { type VariationsSeed } from '../../proto/generated/variations_seed';
-import * as study_filter_utils from './study_filter_utils';
+import ProcessedStudy from './processed_study';
 import * as study_json_utils from './study_json_utils';
 
 // Validate a seed for common errors. Throws an error if any is found.
@@ -27,7 +27,9 @@ export function validateSeedReturnErrors(seed: VariationsSeed): string[] {
 function checkOverlappingStudies(seed: VariationsSeed): string[] {
   const errors: string[] = [];
   // Create a map from feature name to studies that use that feature.
-  const featureNamesToStudies = new DefaultMap<string, Study[]>(() => []);
+  const featureNamesToStudies = new DefaultMap<string, ProcessedStudy[]>(
+    () => [],
+  );
   for (const study of seed.study) {
     const usedFeatureNames = new Set<string>();
     for (const experiment of study.experiment) {
@@ -43,122 +45,36 @@ function checkOverlappingStudies(seed: VariationsSeed): string[] {
     }
 
     for (const usedFeatureName of usedFeatureNames) {
-      featureNamesToStudies.get(usedFeatureName).push(study);
+      featureNamesToStudies
+        .get(usedFeatureName)
+        .push(new ProcessedStudy(study));
     }
   }
 
-  // Date range cache for each study.
-  const studyDateRanges = new DefaultMap<Study, study_filter_utils.DateRange>(
-    study_filter_utils.getStudyDateRange,
-  );
-
-  // Version range cache for each study.
-  const studyVersionRanges = new DefaultMap<
-    Study,
-    study_filter_utils.VersionRange
-  >(study_filter_utils.getStudyVersionRange);
-
-  // OS version range cache for each study.
-  const studyOsVersionRanges = new DefaultMap<
-    Study,
-    study_filter_utils.VersionRange
-  >(study_filter_utils.getStudyOsVersionRange);
-
   const filterIntersectCheckers = createStudyFilterIntersectCheckers({
-    start_date: (study1: Study, study2: Study) =>
-      doesDateRangeIntersect(
-        studyDateRanges.get(study1),
-        studyDateRanges.get(study2),
-      ),
-    end_date: () => true, // Handled by start_date.
-    min_version: (study1: Study, study2: Study) =>
-      doesVersionRangeIntersect(
-        studyVersionRanges.get(study1),
-        studyVersionRanges.get(study2),
-      ),
-    max_version: () => true, // Handled by min_version.
-    min_os_version: (study1: Study, study2: Study) =>
-      doesVersionRangeIntersect(
-        studyOsVersionRanges.get(study1),
-        studyOsVersionRanges.get(study2),
-      ),
-    max_os_version: () => true, // Handled by min_os_version.
-    channel: (study1: Study, study2: Study) =>
-      doesFilterArrayIntersect(study1.filter?.channel, study2.filter?.channel),
-    platform: (study1: Study, study2: Study) =>
-      doesFilterArrayIntersect(
-        study1.filter?.platform,
-        study2.filter?.platform,
-      ),
-    locale: (study1: Study, study2: Study) =>
-      doesFilterArrayIntersect(study1.filter?.locale, study2.filter?.locale),
-    exclude_locale: (study1: Study, study2: Study) =>
-      doesFilterArrayIntersect(
-        study1.filter?.exclude_locale,
-        study2.filter?.exclude_locale,
-      ),
-    form_factor: (study1: Study, study2: Study) =>
-      doesFilterArrayIntersect(
-        study1.filter?.form_factor,
-        study2.filter?.form_factor,
-      ),
-    exclude_form_factor: (study1: Study, study2: Study) =>
-      doesFilterArrayIntersect(
-        study1.filter?.exclude_form_factor,
-        study2.filter?.exclude_form_factor,
-      ),
-    hardware_class: (study1: Study, study2: Study) =>
-      doesFilterArrayIntersect(
-        study1.filter?.hardware_class,
-        study2.filter?.hardware_class,
-      ),
-    exclude_hardware_class: (study1: Study, study2: Study) =>
-      doesFilterArrayIntersect(
-        study1.filter?.exclude_hardware_class,
-        study2.filter?.exclude_hardware_class,
-      ),
-    country: (study1: Study, study2: Study) =>
-      doesFilterArrayIntersect(study1.filter?.country, study2.filter?.country),
-    exclude_country: (study1: Study, study2: Study) =>
-      doesFilterArrayIntersect(
-        study1.filter?.exclude_country,
-        study2.filter?.exclude_country,
-      ),
-    is_low_end_device: (study1: Study, study2: Study) =>
-      doesFilterValueIntersect(
-        study1.filter?.is_low_end_device,
-        study2.filter?.is_low_end_device,
-      ),
-    is_enterprise: (study1: Study, study2: Study) =>
-      doesFilterValueIntersect(
-        study1.filter?.is_enterprise,
-        study2.filter?.is_enterprise,
-      ),
-    policy_restriction: (study1: Study, study2: Study) =>
-      doesFilterValueIntersect(
-        study1.filter?.policy_restriction,
-        study2.filter?.policy_restriction,
-      ),
-    cpu_architecture: (study1: Study, study2: Study) =>
-      doesFilterArrayIntersect(
-        study1.filter?.cpu_architecture,
-        study2.filter?.cpu_architecture,
-      ),
-    exclude_cpu_architecture: (study1: Study, study2: Study) =>
-      doesFilterArrayIntersect(
-        study1.filter?.exclude_cpu_architecture,
-        study2.filter?.exclude_cpu_architecture,
-      ),
-    google_group: (study1: Study, study2: Study) =>
-      doesFilterArrayIntersect(
-        study1.filter?.google_group,
-        study2.filter?.google_group,
-      ),
-    exclude_google_group: (study1: Study, study2: Study) =>
-      doesFilterArrayIntersect(
-        study1.filter?.exclude_google_group,
-        study2.filter?.exclude_google_group,
-      ),
+    start_date: doesDateRangeIntersect,
+    end_date: () => true, // Checked by start_date.
+    min_version: doesVersionRangeIntersect,
+    max_version: () => true, // Checked by min_version.
+    min_os_version: doesVersionRangeIntersect,
+    max_os_version: () => true, // Checked by min_os_version.
+    channel: doesFilterArrayIntersect,
+    platform: doesFilterArrayIntersect,
+    locale: doesFilterArrayWithExcludesIntersect,
+    exclude_locale: () => true, // Checked by locale.
+    form_factor: doesFilterArrayWithExcludesIntersect,
+    exclude_form_factor: () => true, // Checked by form_factor.
+    hardware_class: doesFilterArrayWithExcludesIntersect,
+    exclude_hardware_class: () => true, // Checked by hardware_class.
+    country: doesFilterArrayWithExcludesIntersect,
+    exclude_country: () => true, // Checked by country.
+    is_low_end_device: doesFilterValueIntersect,
+    is_enterprise: doesFilterValueIntersect,
+    policy_restriction: doesFilterValueIntersect,
+    cpu_architecture: doesFilterArrayWithExcludesIntersect,
+    exclude_cpu_architecture: () => true, // Checked by cpu_architecture.
+    google_group: doesFilterArrayWithExcludesIntersect,
+    exclude_google_group: () => true, // Checked by google_group.
   });
 
   for (const [featureName, studies] of featureNamesToStudies.entries()) {
@@ -169,17 +85,19 @@ function checkOverlappingStudies(seed: VariationsSeed): string[] {
 
         // Check if any of the filters differ between the two studies.
         const hasFilterDifference = Object.keys(filterIntersectCheckers).some(
-          (key) =>
-            !filterIntersectCheckers[
-              key as keyof typeof filterIntersectCheckers
-            ](study1, study2),
+          (key) => {
+            return (
+              (filterIntersectCheckers as any)[key](study1, study2, key) ===
+              false
+            );
+          },
         );
 
         // If there is no filter difference, the studies overlap in all
         // properties, meaning the same feature is used in both.
         if (!hasFilterDifference) {
           errors.push(
-            `Feature ${featureName} overlaps in studies. Check your filters:\n${study_json_utils.stringifyStudyArray([study1, study2])}`,
+            `Feature ${featureName} overlaps in studies. Check your filters:\n${study_json_utils.stringifyStudyArray([study1.study, study2.study])}`,
           );
         }
       }
@@ -189,12 +107,38 @@ function checkOverlappingStudies(seed: VariationsSeed): string[] {
   return errors;
 }
 
+// Type helper to extract keys from Study_Filter that are arrays and do not have
+// an "exclude_" field.
+type FilterKeysWithoutExclude<T = Required<Study_Filter>> = {
+  [K in keyof T]: T[K] extends any[]
+    ? `exclude_${string & K}` extends keyof T
+      ? never
+      : K
+    : never;
+}[keyof T];
+
+// Type helper to extract keys from Study_Filter that are arrays and have an
+// "exclude_" field.
+type FilterKeysWithExclude<T = Required<Study_Filter>> = {
+  [K in keyof T]: T[K] extends any[]
+    ? `exclude_${string & K}` extends keyof T
+      ? K
+      : never
+    : never;
+}[keyof T];
+
+// Type helper to extract keys from Study_Filter that are not arrays.
+type FilterKeyValues<T = Required<Study_Filter>> = {
+  [K in keyof T]: T[K] extends any[] ? never : K;
+}[keyof T];
+
 // Type helper to ensure that all filters of Study_Filter structure are listed.
 function createStudyFilterIntersectCheckers<
   T extends {
     [K in keyof Required<Study_Filter>]: (
-      study1: Study,
-      study2: Study,
+      study1: ProcessedStudy,
+      study2: ProcessedStudy,
+      field: K,
     ) => boolean;
   },
 >(checkers: T): T {
@@ -202,11 +146,24 @@ function createStudyFilterIntersectCheckers<
 }
 
 function doesVersionRangeIntersect(
-  range1: study_filter_utils.VersionRange,
-  range2: study_filter_utils.VersionRange,
+  study1: ProcessedStudy,
+  study2: ProcessedStudy,
+  field: 'min_version' | 'min_os_version',
 ): boolean {
-  const [start1, end1] = range1;
-  const [start2, end2] = range2;
+  return doesVersionFieldRangeIntersect(
+    study1,
+    study2,
+    field === 'min_version' ? 'version_range' : 'os_version_range',
+  );
+}
+
+function doesVersionFieldRangeIntersect(
+  study1: ProcessedStudy,
+  study2: ProcessedStudy,
+  field: 'version_range' | 'os_version_range',
+): boolean {
+  const [start1, end1] = study1[field];
+  const [start2, end2] = study2[field];
 
   if (start1 !== undefined && end2 !== undefined && start1.compare(end2) > 0) {
     // Start of range1 is after end of range2, so ranges do not intersect
@@ -223,11 +180,12 @@ function doesVersionRangeIntersect(
 }
 
 function doesDateRangeIntersect(
-  range1: [bigint?, bigint?],
-  range2: [bigint?, bigint?],
+  study1: ProcessedStudy,
+  study2: ProcessedStudy,
+  field: 'start_date',
 ): boolean {
-  const [start1, end1] = range1;
-  const [start2, end2] = range2;
+  const [start1, end1] = study1.date_range;
+  const [start2, end2] = study2.date_range;
 
   if (start1 !== undefined && end2 !== undefined && start1 > end2) {
     // Start of range1 is after end of range2, so ranges do not intersect
@@ -243,7 +201,14 @@ function doesDateRangeIntersect(
   return true;
 }
 
-function doesFilterArrayIntersect<V>(a?: V[], b?: V[]): boolean {
+function doesFilterArrayIntersect<V>(
+  study1: ProcessedStudy,
+  study2: ProcessedStudy,
+  field: FilterKeysWithoutExclude,
+): boolean {
+  const a = study1.study.filter?.[field] as V[] | undefined;
+  const b = study2.study.filter?.[field] as V[] | undefined;
+
   if (a === undefined || b === undefined || a.length === 0 || b.length === 0) {
     return true;
   }
@@ -251,7 +216,72 @@ function doesFilterArrayIntersect<V>(a?: V[], b?: V[]): boolean {
   return a.some((item) => b.includes(item));
 }
 
-function doesFilterValueIntersect<V>(v1?: V, v2?: V): boolean {
+function doesFilterArrayWithExcludesIntersect<V>(
+  study1: ProcessedStudy,
+  study2: ProcessedStudy,
+  field: FilterKeysWithExclude,
+): boolean {
+  function getFilterArray(
+    study: ProcessedStudy,
+    field: keyof Study_Filter,
+  ): V[] {
+    if (study.study.filter === undefined) {
+      return [];
+    }
+    const arrayField = study.study.filter[field];
+    if (arrayField === undefined) {
+      return [];
+    }
+    return arrayField as V[];
+  }
+
+  const study1Filter = getFilterArray(study1, field);
+  const study1ExcludeFilter = getFilterArray(study1, `exclude_${field}`);
+  const study2Filter = getFilterArray(study2, field);
+  const study2ExcludeFilter = getFilterArray(study2, `exclude_${field}`);
+
+  // If one of the studies has no filter, it means it applies to all values.
+  if (
+    (study1Filter.length === 0 && study1ExcludeFilter.length === 0) ||
+    (study2Filter.length === 0 && study2ExcludeFilter.length === 0)
+  ) {
+    return true;
+  }
+
+  // If both studies have include filters, they intersect if at least one value
+  // is in common.
+  if (study1Filter.length > 0 && study2Filter.length > 0) {
+    return study1Filter.some((locale) => study2Filter.includes(locale));
+  }
+
+  // If first study has an include filter and second study has an exclude
+  // filter, the studies won't intersect if all include values are in the
+  // exclude filter.
+  if (study1Filter.length > 0 && study2ExcludeFilter.length > 0) {
+    return !study1Filter.every((locale) =>
+      study2ExcludeFilter.includes(locale),
+    );
+  }
+
+  // Same goes for the other way around.
+  if (study1ExcludeFilter.length > 0 && study2Filter.length > 0) {
+    return !study2Filter.every((locale) =>
+      study1ExcludeFilter.includes(locale),
+    );
+  }
+
+  // If both studies have exclude filters, we assume they intersect.
+  return study1ExcludeFilter.length !== 0 && study2ExcludeFilter.length !== 0;
+}
+
+function doesFilterValueIntersect<V>(
+  study1: ProcessedStudy,
+  study2: ProcessedStudy,
+  field: FilterKeyValues,
+): boolean {
+  const v1 = study1.study.filter?.[field] as V | undefined;
+  const v2 = study2.study.filter?.[field] as V | undefined;
+
   if (v1 === undefined && v2 === undefined) {
     return true;
   }
