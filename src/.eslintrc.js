47181ba5e1e4f00e1e0f8b47bcdf4a03e2afa4e5
Mikhail Atuchin <matuchin@brave.com>
2024-08-20 16:54:29 +0700

    Remove some exclusions

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -35,13 +35,11 @@ module.exports = {
     },
   ],
   rules: {
-    '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unsafe-member-access': 'off',
     '@typescript-eslint/no-unsafe-argument': 'off',
     '@typescript-eslint/no-unsafe-call': 'off',
     '@typescript-eslint/no-unsafe-assignment': 'off',
-    '@typescript-eslint/require-await': 'off',
 
     'licenses/header': [
       2,
--- src/scripts/generate_proto_ts.ts
+++ src/scripts/generate_proto_ts.ts
@@ -29,7 +29,7 @@ interface Options {
   generate_patch?: true;
 }
 
-async function main(options: Options) {
+function main(options: Options) {
   if (options.generate_patch) {
     generateStudyProtoPatch();
     return;
