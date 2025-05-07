0160e0251feab4afc1c7c93e7a0631b9eb09b3fb
Mikhail <matuchin@brave.com>
2025-05-07 16:10:59 +0400

    Remove the legacy python seed generator (#1387)
    In production we use `seed_tools` since October:
    https://github.com/brave/devops/pull/12606/files
    
    I believe it's time to drop the old generator.

--- src/seed_tools/seed_tools.ts
+++ src/seed_tools/seed_tools.ts
@@ -5,7 +5,6 @@
 
 import { program } from '@commander-js/extra-typings';
 
-import compare_python_gen from './commands/compare_python_gen';
 import compare_seeds from './commands/compare_seeds';
 import create from './commands/create';
 import lint from './commands/lint';
@@ -14,7 +13,6 @@ import split_seed_json from './commands/split_seed_json';
 program
   .name('seed_tools')
   .description('Seed tools for manipulating study files.')
-  .addCommand(compare_python_gen())
   .addCommand(compare_seeds())
   .addCommand(create())
   .addCommand(lint())
